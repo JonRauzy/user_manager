8ed81e47dd8968ed3c44ac153d160b58aad4ba42
jonrauzy <jonathan.rauzy@gmail.com>
2026-09-13 12:04:34 +0200

    Key replaced by SecretKey

--- src/main/java/com/jon/user_manager/util/security/JwtUtils.java
+++ src/main/java/com/jon/user_manager/util/security/JwtUtils.java
@@ -9,40 +9,38 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 
 @Component
 public class JwtUtils {
-    private final Key key;
+    private final SecretKey secretKey;
     private final long expiration;
 
-    @Value("${app.jwt.secret}")
-    private String secretKey;
-
-    JwtUtils(@Value("${app.jwt.secret}")String secret, @Value("${app.jwt.refresh-expiration}") long expiration) {
-        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    JwtUtils(
+            @Value("${app.jwt.secret}")String secret,
+            @Value("${app.jwt.expiration}") long expiration
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         this.expiration = expiration;
     }
 
     private Jws<Claims> parse(String token) {
         return Jwts.parser()
-                .verifyWith(getSigningKey())
+                .verifyWith(secretKey)
                 .build()
                 .parseSignedClaims(token);
     }
 
     public String generateToken(String subject, Map<String, Object> claims) {
         Date now = new Date();
-        Date expire = new Date(now.getTime() + expiration);
 
         return Jwts.builder()
                 .claims(claims)
                 .subject(subject)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(getSigningKey(), Jwts.SIG.HS256)
+                .expiration(new Date(now.getTime() + expiration))
+                .signWith(secretKey)
                 .compact();
     }
 
@@ -55,11 +53,6 @@ public class JwtUtils {
         Claims body = parse(token).getPayload();
         //TODO check it in the debugger
         return body.getSubject();
-    }
-
-    private SecretKey getSigningKey() {
-        byte[] keyBites = secretKey.getBytes();
-        return Keys.hmacShaKeyFor(keyBites);
     }
 
 }
